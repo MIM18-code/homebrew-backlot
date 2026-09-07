31d2929e738ea69304169c64f0f355a386f19b49
Joshua Land <mindinmotionpro@gmail.com>
2026-09-06 22:42:47 -0400

    Cask for Backlot 0.3.0

--- Casks/backlot.rb
+++ Casks/backlot.rb
@@ -1,6 +1,6 @@
 cask "backlot" do
-  version "0.2.1"
-  sha256 "bef69947865a1d432db2fa8ff903cf26147b3bcada4e52c7abd831e1043afd08"
+  version "0.3.0"
+  sha256 "26bbff49a1b9ae7eaf42a5bc92d689080c6c8d45ac878aa8ab605e8e12c0ac11"
 
   url "https://github.com/MIM18-code/homebrew-backlot/releases/download/v#{version}/Backlot-#{version}.dmg"
   name "Backlot"
